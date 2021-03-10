9c7b882979ee94663b2cf8d124a1b5f07b35e8d4
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-10 19:01:29 +0800

    Add exercises 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Filter, PersonForm, Persons } from "./components";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState(null);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterPersons, setFilterPersons] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((res) => {
+      const data = res.data;
+      setPersons(data);
+    });
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -3,7 +3,7 @@ import React from "react";
 const Persons = ({ persons }) => {
   return (
     <>
-      {persons.map((el) => (
+      {persons?.map((el) => (
         <p key={el.name}>
           {el.name} {el.number}
         </p>
