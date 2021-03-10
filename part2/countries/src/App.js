4ff13b5e4317d28368a548bbfb207c6b5a9e4e2b
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-10 21:01:34 +0800

    Add exercises 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,66 +1,85 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 
-const Countries = ({ countries }) => {
+const Countries = ({ countries, action }) => {
   return (
     <>
-      {countries.map((country) => (
-        <p key={country.cioc}>{country.name}</p>
-      ))}
+      {countries.map((country) => {
+        return (
+          <Fragment key={country.cioc}>
+            <span>{country.name} </span>
+            <button onClick={() => action(country)}>show</button> <br />
+          </Fragment>
+        );
+      })}
     </>
   );
 };
 
-const Country = ({ countries }) => {
+const Country = ({ country }) => {
   return (
     <>
-      <h1>{countries[0].name}</h1>
-      <p>capital {countries[0].capital}</p>
-      <p>population {countries[0].population}</p>
+      <h1>{country.name}</h1>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
       <h2>languages</h2>
       <ul>
-        {countries[0].languages.map((lang) => (
+        {country.languages.map((lang) => (
           <li key={lang.name}>{lang.name}</li>
         ))}
       </ul>
-      <img
-        alt="flag"
-        style={{ width: 90, height: 90 }}
-        src={countries[0].flag}
-      />
+      <img alt="flag" style={{ width: 90, height: 90 }} src={country.flag} />
     </>
+  );
+};
+
+const ListCountries = ({ search }) => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState(null);
+
+  const showDetails = (country) => {
+    setCountry(country);
+  };
+
+  useEffect(() => {
+    if (search !== "") {
+      axios
+        .get(`https://restcountries.eu/rest/v2/name/${search}`)
+        .then((res) => {
+          const data = res.data;
+          if (data.length === 1) {
+            setCountry(data[0]);
+          }
+          setCountries(data);
+        });
+    }
+  }, [search]);
+
+  return (
+    <div>
+      {countries.length > 10 && <p>Too many matches, specify another filter</p>}
+      {countries.length < 10 && countries.length !== 1 && (
+        <Countries countries={countries} action={showDetails} />
+      )}
+      {!!country && <Country country={country} />}
+    </div>
   );
 };
 
 const App = () => {
   const [search, setSearch] = useState("");
-  const [countries, setCountries] = useState([]);
 
   const handleSearchChange = (e) => {
     const searchValue = e.target.value.toLowerCase();
     setSearch(searchValue);
   };
 
-  useEffect(() => {
-    axios.get(`https://restcountries.eu/rest/v2/name/${search}`).then((res) => {
-      const data = res.data;
-      setCountries(data);
-    });
-  }, [search]);
-
   return (
     <div>
       <div>
         find countries <input onChange={handleSearchChange} value={search} />
       </div>
-
-      <div>
-        {countries.length > 10 && (
-          <p>Too many matches, specify another filter</p>
-        )}
-        {countries.length === 1 && <Country countries={countries} />}
-        {countries.length < 10 && <Countries countries={countries} />}
-      </div>
+      <ListCountries search={search} />
     </div>
   );
 };
