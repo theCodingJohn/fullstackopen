e883c3da4521cd61ff900d7362e493874103b507
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-12 15:10:31 +0800

    Add exercise 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = parseInt(process.argv[4]);
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const url = `mongodb+srv://john:${password}@cluster0.h8g9e.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length > 3) {
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((el) => {
+      console.log(`${el.name} ${el.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
