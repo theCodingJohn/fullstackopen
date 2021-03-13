34e381210b8be424b6c76c871b80fe3bba585fe7
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-13 14:26:28 +0800

    Add exercise 4.1

---
+++ part4/blog/.eslintrc.cjs
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+  },
+};
