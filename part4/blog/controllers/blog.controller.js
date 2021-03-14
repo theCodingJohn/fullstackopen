b50a1cf5afb68e7ac147cc271cc48d0f3de05509
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-14 22:59:16 +0800

    Add exercise 4.8

--- part4/blog/controllers/blog.controller.js
+++ part4/blog/controllers/blog.controller.js
@@ -3,18 +3,16 @@ import Blog from "../modules/blog.module.js";
 
 const blogsRouter = express.Router();
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  const result = await blog.save();
+  response.status(201).json(result);
 });
 
 export default blogsRouter;
---
+++ part4/blog/tests/blogs_api.test.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import supertest from "supertest";
+import app from "../app.js";
+import helper from "./test_helper.js";
+
+import Blog from "../modules/blog.module.js";
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
+
+const api = supertest(app);
+
+test("the amount of blogs returned is correct", async () => {
+  const initialBLogs = await helper.notesInDB();
+
+  const response = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+  expect(response.body).toHaveLength(initialBLogs.length);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
