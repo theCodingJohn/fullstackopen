513660f7fdc6410e605922cdaefb1c875c4b80e3
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-20 15:33:22 +0800

    Add exercise 6.2

--- part6/unicafe/src/index.js
+++ part6/unicafe/src/index.js
@@ -1,69 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import counterReducer from "./reducer/unicafeReducer";
+import { createStore } from "redux";
 
-const Button = ({ name, action }) => {
-  return <button onClick={action}>{name}</button>;
-};
-
-const Statistics = ({ stats: { good, bad, neutral } }) => {
-  const totalFeedback = good + neutral + bad;
-  const average = (good - bad) / totalFeedback;
-  const positive = (good / totalFeedback) * 100;
-
-  return (
-    <>
-      <h2>statistics</h2>
-      <table>
-        <tbody>
-          <Statistic text="good" value={good} />
-          <Statistic text="neutral" value={neutral} />
-          <Statistic text="bad" value={bad} />
-          <Statistic text="all" value={totalFeedback} />
-          <Statistic text="average" value={average} />
-          <Statistic text="positive" value={positive} />
-        </tbody>
-      </table>
-    </>
-  );
-};
-
-const Statistic = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  );
-};
+const store = createStore(counterReducer);
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const stats = {
-    good: good,
-    neutral: neutral,
-    bad: bad,
-  };
-
-  const addGood = () => setGood(good + 1);
-  const addNeutral = () => setNeutral(neutral + 1);
-  const addBad = () => setBad(bad + 1);
+  const addGood = () => store.dispatch({ type: "GOOD" });
+  const addNeutral = () => store.dispatch({ type: "OK" });
+  const addBad = () => store.dispatch({ type: "BAD" });
+  const reset = () => store.dispatch({ type: "ZERO" });
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button name="good" value={good} action={addGood} />
-      <Button name="neutral" value={neutral} action={addNeutral} />
-      <Button name="bad" value={bad} action={addBad} />
-      {good > 0 || bad > 0 || neutral > 0 ? (
-        <Statistics stats={stats} />
-      ) : (
-        <p>No feedback given</p>
-      )}
+      <button onClick={addGood}>good</button>
+      <button onClick={addNeutral}>neutral</button>
+      <button onClick={addBad}>bad</button>
+      <button onClick={reset}>zero</button>
+      <div>
+        good <strong>{store.getState().good}</strong>
+      </div>
+      <div>
+        neutral <strong>{store.getState().ok}</strong>
+      </div>
+      <div>
+        bad <strong>{store.getState().bad}</strong>
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+renderApp();
+store.subscribe(renderApp);
--- part6/unicafe/src/reducer/unicafeReducer.js
+++ part6/unicafe/src/reducer/unicafeReducer.js
@@ -13,6 +13,8 @@ const counterReducer = (state = initialState, action) => {
       return { ...state, ok: state.ok + 1 };
     case "BAD":
       return { ...state, bad: state.bad + 1 };
+    case "ZERO":
+      return initialState;
     default:
       return state;
   }
