bea71155f91625ae1f5ce3add4113a3c3992d01d
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-23 20:25:37 +0800

    Add exercise 7.19.-7.21.

--- part7/bloglist/client/src/components/Nav.jsx
+++ part7/bloglist/client/src/components/Nav.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 80px 80px 300px;
+  align-items: center;
+  height: 70px;
+  background-color: lightblue;
+`;
+
+const Button = styled.button`
+  background-color: "white";
+  border: none;
+  margin-left: 30px;
+  transform: scale(1.2);
+  border-radius: 3px;
+  cursor: pointer;
+`;
 
 const Nav = ({ logoutUser, user }) => {
 
   return (
-    <div>
+    <Container>
       <Link to="/blogs">blogs</Link>
       <Link to="/users">users</Link>
       {
         user &&
-        <div>
+        <span>
           <strong>{user.name}</strong> is logged in
-          <button onClick={logoutUser}>logout</button>
-        </div>
+          <Button onClick={logoutUser}>logout</Button>
+        </span>
       }
-    </div>
+    </Container>
   );
 };
 
