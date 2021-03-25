efabc829731207ac519cbbd5c971b414eda989e8
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-25 11:30:32 +0800

    Add exercise 8.5

--- part8/library/index.js
+++ part8/library/index.js
@@ -87,7 +87,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -112,8 +112,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books;
-      return books.filter((book) => book.author === args.author);
+      if (!args) return books;
+
+      if (args.author)
+        return books.filter((book) => book.author === args.author);
+
+      if (args.genre)
+        return books.filter((book) => book.genres.includes(args.genre));
     },
     allAuthors: () => {
       return authors.map((author) => {
