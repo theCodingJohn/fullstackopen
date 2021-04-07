90f788c1b8b951ef40ddbc548dab55904a2819c4
John Carlo Cunanan <codingjohn@outlook.com>
2021-04-07 17:58:27 +0800

    Add exercise 8.18

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,34 +1,47 @@
-
-import React, { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import React, { useState, useEffect } from "react";
+import { useApolloClient } from "@apollo/client";
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
+  const client = useApolloClient();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("user-token");
+    if (token) {
+      setToken(token);
+    }
+  }, []); // eslint-disable-line
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        {token && <button onClick={() => setPage("add")}>add book</button>}
+        {!token && <button onClick={() => setPage("login")}>login</button>}
+        {token && <button onClick={logout}>logout</button>}
       </div>
 
-      <Authors
-        show={page === 'authors'}
-      />
+      <Authors show={page === "authors"} />
 
-      <Books
-        show={page === 'books'}
-      />
+      <Books show={page === "books"} />
 
-      <NewBook
-        show={page === 'add'}
-      />
+      <NewBook show={page === "add"} />
 
+      <LoginForm show={page === "login"} token={token} setToken={setToken} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
